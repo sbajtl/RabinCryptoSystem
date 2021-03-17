f2a2631c68af4ba37d47766313c72fb636fc3209
Saša Bajtl <s.bajtl@systec.danieli.com>
2021-03-17 02:56:07 +0100

    Changes...

--- main.py
+++ main.py
@@ -22,11 +22,18 @@ class Rabin:
     def decrypt(self):
         print("decrypt")
 
+    '''
+    The keys for the Rabin crypto system are generated as follows:
+    Choose two large distinct prime numbers p and q such that p≡3mod4 and q≡3mod4.
+    Compute n=pq.
+    Then n is the public key and the pair (p,q) is the private key.
+    '''
     def generate_key(self):
         p = self.__generate_prime_number()
         q = self.__generate_prime_number()
         if p == q:
-            raise Exception("Numbers cannot be same!")
+            print(p, q, "Numbers cannot be same! Generating again...")
+            return self.generate_key()
         n = p * q
         return n, (p, q)
 
