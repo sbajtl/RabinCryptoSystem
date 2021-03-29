723b6a49e9e3955239adf4ee3f4580207b78bea0
Saša Bajtl <s.bajtl@systec.danieli.com>
2021-03-30 01:29:53 +0200

    Prerađen skroz!

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ class Rabin:
     __bits = 0
     __public_key = 0
     __private_key = 0
+    __message = ""
 
-    # default constructor
     def __init__(self, bits):
         self.__bits = bits
 
@@ -24,94 +24,86 @@ class Rabin:
         self.__private_key = k
 
     def __generate_prime_number(self):
+        """
+        Generiranje velikih prim brojeva.
+        :return:
+        :rtype:
+        """
         while True:
-            prime_number = Cryptodome.Util.number.getPrime(self.__bits, randfunc=Cryptodome.Random.get_random_bytes)
+            prime_number = Cryptodome.Util.number.getPrime(self.__bits)
             if (prime_number % 4) == 3:
                 break
         return prime_number
 
-    @staticmethod
-    def __convert_message_to_int(message):
+    def __convert_message(self, message):
+        """
+        Konvertira poruku u bit string (binarni) i dodaje zadnjih 6 bitova na kraj (zalihost).
+        :param message:
+        :type message: string ili int
+        :return: int_output
+        :rtype: int
+        """
+        converted = self.__convert_by_type(message)
+        bit_string = bin(converted)  # convert to a bit string
+        output = bit_string + bit_string[-6:]  # pad the last 6 bits to the end
+        int_output = int(output, 2)  # convert back to integer
+        return int_output
 
-        # byte_array = Cryptodome.Util.number.bytes_to_long(message.encode('utf-8'))  #message.encode()
-        # # binary_int = int.from_bytes(byte_array, "big")
-        # binary_string = bin(byte_array)  # convert to a bit string
-        # output = binary_string + binary_string[-6:]  # pad the last 16 bits to the end
-        # int_output = int(output, 2)  # convert back to integer
+    def __extended_euclidean(self, a, b):
+        """
 
-        binary_str = bin(message)  # convert to a bit string
-        output = binary_str + binary_str[-6:]  # pad the last 16 bits to the end
-        return int(output, 2)  # convert back to integer
-
-    def egcd(self, a, b):
+        :param a:
+        :type a:
+        :param b:
+        :type b:
+        :return:
+        :rtype:
+        """
         if a == 0:
             return b, 0, 1
         else:
-            gcd, y, x = self.egcd(b % a, a)
+            gcd, y, x = self.__extended_euclidean(b % a, a)
             return gcd, x - (b // a) * y, y
 
-    '''
-    A message M can be encrypted by first converting it to a number m<n using a reversible mapping, 
-    then computing c=m^2mod n. The ciphertext is c.
-    '''
-    def encrypt(self, message):
-        # c = m^2 mod n
-        message = self.__convert_message_to_int(message)
-        q = message ** 2
-        enc = q % self.get_public_key()
-        return enc
-
-    def decrypt(self, cipher):
-        n = self.get_public_key()
-        p = self.get_private_key()[0]
-        q = self.get_private_key()[1]
-
-        r = pow(cipher, (p + 1) // 4, p)
-        # p2 = p - p1
-
-        s = pow(cipher, (q + 1) // 4, q)
-        # q2 = q - q1
-
-        ext = self.egcd(p, q)
-        a = ext[1]
-        b = ext[2]
-
-        # x = (aps + bqr)(mod n)
-        # y = (aps - bqr)(mod n)
-        x = ((a * p * s + b * q * r) % n)
-        y = ((a * p * s - b * q * r) % n)
-
-        lst = [x, n - x, y, n - y]
-        print(lst)
-
-        plaintext = self.__choose(lst)
-
-        string = bin(plaintext)
-        string = string[:-6]
-        plaintext = int(string, 2)
-
-        return plaintext
-
-    # decide which answer to choose
     @staticmethod
-    def __choose(lst):
+    def __convert_by_type(message):
+        """
+        Za sada se provjerava samo da li je string i napravi konverzija, inače ako je int vrati poruku kakva jest.
+        :param message:
+        :type message:
+        :return:
+        :rtype:
+        """
+        if isinstance(message, str):
+            message = Cryptodome.Util.number.bytes_to_long(message.encode('utf-8'))
+        else:
+            message = message
+        return message
 
-        for i in lst:
+    @staticmethod
+    def __select_solution(solutions):
+        """
+        Odabir između 4 moguća rješenja.
+        :param solutions:
+        :type solutions:
+        :return:
+        :rtype:
+        """
+        for i in solutions:
             binary = bin(i)
-            append = binary[-6:]  # take the last 16 bits
-            binary = binary[:-6]  # remove the last 16 bits
+            append = binary[-6:]  # take the last 6 bits
+            binary = binary[:-6]  # remove the last 6 bits
+
             if append == binary[-6:]:
                 return i
         return
 
-    '''
-    The keys for the Rabin crypto system are generated as follows:
-    Choose two large distinct prime numbers p and q such that p≡3mod4 and q≡3mod4.
-    Compute n=pq.
-    Then n is the public key and the pair (p,q) is the private key.
-    '''
-
     def generate_key(self):
+        """
+        Generiranje javnog i privatnog ključa.
+        :return:
+        :rtype:
+        """
         p = self.__generate_prime_number()
         q = self.__generate_prime_number()
         if p == q:
@@ -121,21 +113,87 @@ class Rabin:
         self.set_public_key(n)
         self.set_private_key((p, q))
 
+    def encrypt(self, message):
+        """
+        Enkripcija
+        :param message:
+        :type message:
+        :return:
+        :rtype:
+        """
+        self.__message = message
+        message = self.__convert_message(message)
+        return pow(message, 2, self.get_public_key())
+
+    def decrypt(self, cipher):
+        """
+        Dekripcija
+        :param cipher:
+        :type cipher:
+        :return:
+        :rtype:
+        """
+        n = self.get_public_key()
+        p = self.get_private_key()[0]
+        q = self.get_private_key()[1]
+
+        r = pow(cipher, (p + 1) // 4, p)
+        s = pow(cipher, (q + 1) // 4, q)
+
+        ext = self.__extended_euclidean(p, q)
+        a = ext[1]
+        b = ext[2]
+
+        # x = (aps + bqr)(mod n)
+        # y = (aps - bqr)(mod n)
+        x = ((a * p * s + b * q * r) % n)
+        y = ((a * p * s - b * q * r) % n)
+
+        solutions = [x, n - x, y, n - y]
+        print("Possible solutions:", solutions)
+
+        plain_text = self.__select_solution(solutions)
+
+        string = bin(plain_text)
+        string = string[:-6]
+        plain_text = int(string, 2)
+
+        decrypted_text = self.__get_decrypted_text(plain_text)
+
+        return decrypted_text
+
+    def __get_decrypted_text(self, plain_text):
+        """
+
+        :param plain_text:
+        :type plain_text:
+        :return:
+        :rtype:
+        """
+        if isinstance(self.__message, str):
+            formatted_text = format(plain_text, 'x')
+            text_decrypted = bytes.fromhex(formatted_text).decode()
+        else:
+            text_decrypted = plain_text
+        return text_decrypted
+
 
 if __name__ == '__main__':
-    rabin = Rabin(20)
+    rabin = Rabin(512)
     rabin.generate_key()
 
-    print("Public key: ", rabin.get_public_key())
-    print("Private key: ", rabin.get_private_key())
+    print("Public key:", rabin.get_public_key())
+    print("Private key:", rabin.get_private_key())
+    print("\n")
 
-    cipher_text = rabin.encrypt(666666)
+    text = "Test nekog teksta besmislenog samo radi Rabina!"
+    cipher_text_string = rabin.encrypt(text)
+    print("Cipher text string:", cipher_text_string)
+    decrypted_text_string = rabin.decrypt(cipher_text_string)
+    print("Decrypted text string:", decrypted_text_string)
+    print("\n")
 
-    print("Cipher text: ", cipher_text)
-    decrypted = rabin.decrypt(cipher_text)
-    print("DECRIPTED", decrypted)
-
-    # st = format(decrypted, 'x')
-    # st2 = format(decrypted, 'x')
-    # print("Decripted text: ", bytes.fromhex(st).decode())
-    # print("Decripted2 text: ", bytes.fromhex(st2).decode())
+    cipher_text_int = rabin.encrypt(123456789)
+    print("Cipher text int:", cipher_text_int)
+    decrypted_text_int = rabin.decrypt(cipher_text_int)
+    print("Decrypted text int:", decrypted_text_int)
