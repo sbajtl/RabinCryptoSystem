bad4f650df382569173046229ec299751f8fa43f
Saša Bajtl <s.bajtl@systec.danieli.com>
2021-03-29 11:49:32 +0200

    Rabin brojevi

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from decimal import Decimal
-
 import Cryptodome.Util.number
 import Cryptodome.Random
 
@@ -25,24 +23,6 @@ class Rabin:
     def set_private_key(self, k):
         self.__private_key = k
 
-    # Find SQROOT in Zp where p = 3 mod 4
-    @staticmethod
-    def __sqrt_p_3_mod_4(a, p):
-        r = pow(a, (p + 1) // 4, p)
-        return r
-
-    # Find SQROOT in Zp where p = 5 mod 8
-    @staticmethod
-    def __sqrt_p_5_mod_8(a, p):
-        d = pow(a, (p - 1) // 4, p)
-        r = 0
-        if d == 1:
-            r = pow(a, (p + 3) // 8, p)
-        elif d == p - 1:
-            r = 2 * a * pow(4 * a, (p - 5) // 8, p) % p
-
-        return r
-
     def __generate_prime_number(self):
         while True:
             prime_number = Cryptodome.Util.number.getPrime(self.__bits, randfunc=Cryptodome.Random.get_random_bytes)
@@ -52,12 +32,16 @@ class Rabin:
 
     @staticmethod
     def __convert_message_to_int(message):
-        byte_array = message.encode()
-        binary_int = int.from_bytes(byte_array, "big")
-        binary_string = bin(binary_int)  # convert to a bit string
-        output = binary_string + binary_string[-16:]  # pad the last 16 bits to the end
-        int_output = int(output, 2)  # convert back to integer
-        return int_output
+
+        # byte_array = Cryptodome.Util.number.bytes_to_long(message.encode('utf-8'))  #message.encode()
+        # # binary_int = int.from_bytes(byte_array, "big")
+        # binary_string = bin(byte_array)  # convert to a bit string
+        # output = binary_string + binary_string[-6:]  # pad the last 16 bits to the end
+        # int_output = int(output, 2)  # convert back to integer
+
+        binary_str = bin(message)  # convert to a bit string
+        output = binary_str + binary_str[-6:]  # pad the last 16 bits to the end
+        return int(output, 2)  # convert back to integer
 
     def egcd(self, a, b):
         if a == 0:
@@ -73,33 +57,37 @@ class Rabin:
     def encrypt(self, message):
         # c = m^2 mod n
         message = self.__convert_message_to_int(message)
-        return message ** 2 % self.get_public_key()
+        q = message ** 2
+        enc = q % self.get_public_key()
+        return enc
 
     def decrypt(self, cipher):
-        r, s = 0, 0
+        n = self.get_public_key()
         p = self.get_private_key()[0]
         q = self.get_private_key()[1]
 
-        if p % 4 == 3:
-            r = self.__sqrt_p_3_mod_4(cipher, p)
-        elif p % 8 == 5:
-            r = self.__sqrt_p_5_mod_8(cipher, p)
-        # for q
-        if q % 4 == 3:
-            s = self.__sqrt_p_3_mod_4(cipher, q)
-        elif q % 8 == 5:
-            s = self.__sqrt_p_5_mod_8(cipher, q)
+        r = pow(cipher, (p + 1) // 4, p)
+        # p2 = p - p1
 
-        gcd, c, d = self.egcd(p, q)
-        n = self.get_public_key()
-        x = (r * d * q + s * c * p) % n
-        y = (r * d * q - s * c * p) % n
+        s = pow(cipher, (q + 1) // 4, q)
+        # q2 = q - q1
+
+        ext = self.egcd(p, q)
+        a = ext[1]
+        b = ext[2]
+
+        # x = (aps + bqr)(mod n)
+        # y = (aps - bqr)(mod n)
+        x = ((a * p * s + b * q * r) % n)
+        y = ((a * p * s - b * q * r) % n)
+
         lst = [x, n - x, y, n - y]
         print(lst)
+
         plaintext = self.__choose(lst)
 
         string = bin(plaintext)
-        string = string[:-16]
+        string = string[:-6]
         plaintext = int(string, 2)
 
         return plaintext
@@ -110,9 +98,9 @@ class Rabin:
 
         for i in lst:
             binary = bin(i)
-            append = binary[-16:]  # take the last 16 bits
-            binary = binary[:-16]  # remove the last 16 bits
-            if append == binary[-16:]:
+            append = binary[-6:]  # take the last 16 bits
+            binary = binary[:-6]  # remove the last 16 bits
+            if append == binary[-6:]:
                 return i
         return
 
@@ -135,13 +123,19 @@ class Rabin:
 
 
 if __name__ == '__main__':
-    rabin = Rabin(120)
+    rabin = Rabin(20)
     rabin.generate_key()
+
     print("Public key: ", rabin.get_public_key())
     print("Private key: ", rabin.get_private_key())
 
-    cipher_text = rabin.encrypt('Pokušaj testiranje ili ...!')
+    cipher_text = rabin.encrypt(666666)
+
     print("Cipher text: ", cipher_text)
     decrypted = rabin.decrypt(cipher_text)
-    st = format(decrypted, 'x')
-    print("Decripted text: ", bytes.fromhex(st).decode())
+    print("DECRIPTED", decrypted)
+
+    # st = format(decrypted, 'x')
+    # st2 = format(decrypted, 'x')
+    # print("Decripted text: ", bytes.fromhex(st).decode())
+    # print("Decripted2 text: ", bytes.fromhex(st2).decode())
